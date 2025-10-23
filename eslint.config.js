8d6ec0384b620dc4d06dfc6732da699b011fb866
taeyun <lwe12f675@gmail.com>
2025-10-23 16:38:08 +0900

    초기 셋팅 및 TailWindCSS 설정

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": "off", // 선언만 하고 사용하지 않는 변수가 있을 때 에러로 표기하는 걸 끔
+      "@typescript-eslint/no-explicit-any": "off", // 명시적으로 any타입 사용시 에러로 표기하는 걸 끔
+    },
+  }
+);
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-amber-300 text-3xl font-bold underline">Hello World</div>
+  );
+}
+
+export default App;
