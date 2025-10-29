c2afe575e52908ddf65928b7f9b3d94c3ed84546
taeyun <lwe12f675@gmail.com>
2025-10-29 16:56:37 +0900

    캐시데이터 다루기 2 - 응답 결과값 이용하기(기존 캐시데이터를 직접 수정해 데이터 추가)

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useCreateTodoMutation = () => {
@@ -9,9 +10,12 @@ export const useCreateTodoMutation = () => {
     mutationFn: createTodo,
     onMutate: () => {}, // 요청이 딱 발송되었을 때 사용되는 이벤트 핸들러
     onSettled: () => {}, // 요청이 종료됐을 때 사용되는 이벤트 핸들러
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    onSuccess: (newTodo) => {
+      // setQueryData를 사용해 Todo가 추가되면 기존 캐시데이터를 직접 수정해 새로운 Todo를 추가하여 데이터 리페칭없이 화면에 렌더링 할 수 있음
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+
+        return [...prevTodos, newTodo];
       });
     }, // 요청이 성공했을 때 사용되는 이벤트 핸들러
     onError: (err) => {
